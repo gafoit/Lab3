5f45a5d04ba9a50bdd443b7e71b428ace0252ed7
Gafoit <daniilgafoit@gmail.com>
2024-10-13 16:00:03 +0300

    init push

--- src/main/java/Laba3/Main.java
+++ src/main/java/Laba3/Main.java
@@ -2,22 +2,35 @@ package Laba3;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.HashSet;
 import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(VisitorsString(ParseFile("P:\\JavaLabs\\Lab3\\src\\main\\java\\Laba3\\books.json")));
-        System.out.println("Visitors Count:"+CountVisitors(ParseFile("P:\\JavaLabs\\Lab3\\src\\main\\java\\Laba3\\books.json")));
-
+        List<Visitor> visitors = ParseFile("P:\\JavaLabs\\Lab3\\src\\main\\java\\Laba3\\books.json");
+        if (visitors != null) {
+            System.out.println("Задание 1:");
+            System.out.println(VisitorsString(visitors));
+            System.out.println("Visitors Count:" + CountVisitors(visitors));
+            System.out.println("\nЗадание 2:");
+            for (Book i: bookSet(visitors)) {
+                System.out.println(i.getTitle()+"\t"+i.getAuthor()+"\t"+i.getYear());
+            }
+            System.out.println("Unique Books: "+bookSet(visitors).size());
+        }
     }
-    public static List<Visitor> ParseFile( String path)
-    {
-        Gson gson=new Gson();
-        try(FileReader reader = new FileReader(path)){
-            Type visitorListType = new TypeToken<List<Visitor>>() {}.getType();
+
+    public static List<Visitor> ParseFile(String path) {
+        Gson gson = new Gson();
+        try (FileReader reader = new FileReader(path)) {
+            Type visitorListType = new TypeToken<List<Visitor>>() {
+            }.getType();
             return gson.fromJson(reader, visitorListType);
         } catch (IOException e) {
             e.printStackTrace();
@@ -25,18 +38,26 @@ public class Main {
         }
     }
 
-    public static int CountVisitors(List<Visitor> visitors)
-    {
+    public static int CountVisitors(List<Visitor> visitors) {
         return visitors.size();
     }
 
-    public static String VisitorsString(List<Visitor> visitors)
-    {
+    public static String VisitorsString(List<Visitor> visitors) {
         String visitorsString = "Name\tSurname\tphone\tisSubscribed\n";
-        for (Visitor i:visitors)
-        {
-            visitorsString += i.getFirstName()+'\t'+i.getLastName()+'\t'+i.getPhoneNumber()+'\t'+i.isSubscribed()+'\n';
+        for (Visitor i : visitors) {
+            visitorsString += i.getFirstName() + '\t' + i.getLastName() + '\t' + i.getPhoneNumber() + '\t' + i.isSubscribed() + '\n';
         }
         return visitorsString;
     }
+
+    public static HashSet<Book> bookSet(List<Visitor> visitors)
+    {
+        HashSet<Book> favBooks = new HashSet<>();
+        for (Visitor i: visitors)
+        {
+            favBooks.addAll(i.getFavoriteBooks());
+        }
+        return favBooks;
+    }
+
 }
